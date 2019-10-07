0764f4c9ba8c0a647bdcb453a3becf12a5372dc2
mcallac <53855008+mcallac@users.noreply.github.com>
2019-10-07 15:37:45 +0200

    Add files via upload

--- js/cartorecol.js
+++ js/cartorecol.js
@@ -1,64 +1,149 @@
-var mymap = L.map('map').setView([48.800, -3.28], 13);
-
-var idPlan=getUrlParam('id');
-
-ajaxGet("http://tome/intranet/testplanrecol/plans/"+idPlan+"/plan.geojson", initialize);
+ajaxGet("http://tome/intranet/testplanrecol/python/configuration.json", main);
 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	id: 'mapbox.streets',
-	maxZoom: 19
-}).addTo(mymap);
+/*
+Je suis arrivé où : Paramétrage des chemisn des isones des diiférents objets du plan dans le ficcher python/configuration.json
+il faut désormais retrouver ce paramérage et l'implémenter dans Leaflet (options pointToLayer)
+Si une icone est paramétré afficher un marker sinon un circleMarker.
 
-function initialize(reponse) {
-	// Transforme la réponse en un tableau d'articles
-	geojsonFeature = JSON.parse(reponse);
-	//L.geoJSON(geojsonFeature).addTo(mymap);
+Il faut utliser un tabeau associatif : exemple :
+
+var arr = { "un" : 1, "deux" : 2, "trois": 3 };
+var y = arr["un"]; --> y=1
+
+*/
+
+
+//Fonction Main
+function main(reponse) {
 	
-	// création d’une couche geoJson qui appelle le fichier « cinema.geojson »
-	//var urlPlanGeoJson="/plans/" + idPlan + "/plan.geojson"
+	configJson = JSON.parse(reponse);
 	
 	
-	/*var recolpoint= $.getJSON(urlPlanGeoJson,function(dataPointPlan)
-	// icone
-	{var iconePointPlan = L.icon({
-	iconUrl : 'img/info.png',
-	iconSize : [19, 21]
+	//On parcourt les tests pour trouver celui qui contient le paramétrage des calques
+	for (keyGrp in controles.tests) {
+		if controles.tests[keyGrp]=="Test2" {
+			for (keyLayer in controles.tests[keyGrp]layers) {
+				for (keyRubrique in controles.tests[keyGrp]layers[keyLayer]) {
+					/*Créer un tableau associatif dont la clé sera le nom du calque et la valeur le chemin de l'icone*/
+				
+				}
+			}
+		}
+	}
+	
+	
+	var iconVanne=L.icon({
+		iconUrl: '../img/aep/vanne.png',
+		iconSize: [21, 12]
 	});
-	// fonction pointToLayer qui ajoute la couche « dataPointPlan » à la carte, selon la symbologie « iconePointPlan », et paramètre la popup
-	L.geoJson(dataPointPlan,{
-	pointToLayer : function(feature,latlng){
-	var marker = L.marker(latlng,{icon: iconePointPlan});
-	marker.bindPopup('<b><u>Description du Point</u></b><br>'
-	+ '<b>Layer : </b>' + feature.properties.Layer + '<br>'
-	+ '<b>RefName : </b>' + feature.properties.RefName + '<br>'
-	);
-	return marker ;
-	}
-	}).addTo(map);
-	});*/
-	
-	var geojsonMarkerOptions = {
-	radius: 8,
-	fillColor: "#ff7800",
-	color: "#000",
-	weight: 1,
-	opacity: 1,
-	fillOpacity: 0.8
-	};
-	
-	
-	var recolpointLayer = new L.GeoJSON.AJAX(geojsonFeature);
-	
-	L.geoJSON(recolpointLayer, {
-	pointToLayer: function (feature, latlng) {
-		return L.circleMarker(latlng, geojsonMarkerOptions);
-	}
-}).addTo(map);
-	
-	
-	
 
+	var iconNonType=L.icon({
+		iconUrl: '../img/aep/nontype.png',
+		iconSize: [11, 18]
+	});
+
+	var mymap = L.map('map').setView([48.800, -3.28], 13);
+
+	var urlstamen = 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png'
+	var urlosm='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+
+	//Couches Fond de plan
+	var mq=L.tileLayer(urlstamen, {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+	})
+
+	var osmcolor=L.tileLayer(urlosm,{
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	id: 'mapbox.streets',
+	maxZoom: 25
+	})
+
+	//Par défaut on affiche le fond de plan OSM
+	osmcolor.addTo(mymap);
+
+
+	//make the map
+	var options = {
+	onEachFeature: function(feature, layer) {
+		if (feature.properties) {
+			layer.bindPopup(Object.keys(feature.properties).map(function(k) {
+				if(k === '__color__'){
+					return;
+				}
+				return k + ": " + feature.properties[k];
+			}).join("<br />"), {
+				maxHeight: 200
+			});
+		}
+	},
+	style: function(feature) {
+		switch(feature.properties.Layer) {
+				case "EAU_BRAN":
+					var co="#ff0000";
+					break;
+				case "EAU_VAN":
+					var co="#0000ff";
+					break;
+				default:
+					var co="#00ff00";
+					break;
+			}
+		console.log("feature.properties.Layer=" + feature.properties.Layer)
+		return {
+			opacity: 1,
+			fillOpacity: 0.7,
+			radius: 6,
+			color:co
+			}
+		},
+	pointToLayer: function(feature, latlng) {
+		switch(feature.properties.Layer) {
+				//case 'EAU_BRAN':
+				//	var co="#ff0000";
+				//	break;
+				case 'EAU_VAN':
+					var ico=iconVanne;
+					break;
+				default:
+					var ico=iconNonType;
+					break;
+			}
+		/*return L.circleMarker(latlng, {
+			opacity: 1,
+			fillOpacity: 0.7,
+			color: co
+			})*/
+		return L.marker(latlng,{
+			icon: ico
+			})
+		}
+	};
+
+		
+	var shpfile = new L.Shapefile('../plans/1/RueGDeGaullePoint.zip',options);
+
+
+	//Controle gestion d'affichage des fonds de plan
+	var fondPlans={
+		"OSM": osmcolor,
+		"Stamen Toner": mq
+		};
+		
+	var couchePlans={
+		"Point": shpfile
+		};
+	var lc = L.control.layers(fondPlans,couchePlans).addTo(mymap);
+
+
+
+
+	shpfile.addTo(mymap);
+	shpfile.once("data:loaded", function() {
+		console.log("finished loaded shapefile");
+	});
 	
 }
+
+
+
--- php/map.php
+++ php/map.php
@@ -12,36 +12,26 @@
 		@import "../css/style_map.css";
 		@import "../css/interrupt.css";
 	</style>
-	
-	<!-- appelle la librairie javascript Leaflet et le fichier CSS Leaflet -->	
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
-		integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-		crossorigin=""/>
-	<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
-		integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
-	crossorigin=""></script>
-	
-	<!-- Lien vers mabibliothèque javascript -->	
-	<script src="../js/ajax.js"></script>
-	
-	<!-- jQuery - indispensable pour utiliser les fichiers geojson -->
-	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-	
-	
-	
+
+	<!-- CSS-->	
+	<link rel="stylesheet" href="../css/leaflet/leaflet.css"/>
 </head>
 
 <body>
-	
 	<div id="calque_titre_feuille">
 		<h1 id="nom_plan">Visualisation du plan de récolement</h1>
 	</div>
-		
 	<div id="map"></div>
-	
-	
+	<!-- Lien vers bibliothèques javascript -->	
+	<script src="../js/leaflet/leaflet.js"></script>
+	<script src="../js/ajax.js"></script>
+	<script src="../js/leaflet/catiline.js"></script>
+	<script src="../js/leaflet/leaflet.shpfile.js"></script>
+	<!-- jQuery - indispensable pour utiliser les fichiers geojson -->
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 	
 	<script src="../js/cartorecol.js"></script>
+	
 
 </body>
 
